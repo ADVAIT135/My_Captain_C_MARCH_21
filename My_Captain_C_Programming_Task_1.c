19d36175f35d1d65a43643e5a60af69e36ccff95
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2021-03-06 13:32:36 +0530

    My_Captain_C_Programming task 1 : Hello World Program
    Hello world program in C, wherein the compiler takes name from the user and prints (Hello, name!)

---
+++ My_Captain_C_Programming_Task_1.c
@@ -0,0 +1,12 @@
+/*My Captain task 1, Hello world program in C */
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    char a[10];
+    printf("Enter your name:- ");
+    scanf("%s",&a);
+    printf("Hello, %s !",a);
+    getch();
+    return 0;
+}
+
